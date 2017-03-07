cc677d829ec66d11a5e18031507c1cce6144a9d5
Shakib <shakib.hossain@psionicinteractive.com>
2017-03-07 13:43:56 +0600

    Change sidemenu and navbar to show relevant information

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>iERP</title>
+    <title>Weather Board</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -37,9 +37,9 @@
                 <!-- Logo -->
                 <a href="{{url('/')}}" class="logo">
                     <!-- mini logo for sidebar mini 50x50 pixels -->
-                    <span class="logo-mini"><b>i</b>ERP</span>
+                    <span class="logo-mini"><b>i</b>WB</span>
                     <!-- logo for regular state and mobile devices -->
-                    <span class="logo-lg"><b>i</b>ERP</span>
+                    <span class="logo-lg"><b>i</b>WB</span>
                 </a>
                 <!-- Header Navbar: style can be found in header.less -->
                 <nav class="navbar navbar-static-top">
@@ -53,108 +53,23 @@
 
                     <div class="navbar-custom-menu">
                         <ul class="nav navbar-nav">
-                            <!-- Messages: style can be found in dropdown.less-->
-                            <li class="dropdown messages-menu">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                    <i class="fa fa-envelope-o"></i>
-                                    <span class="label label-success">4</span>
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li class="header">You have 4 messages</li>
-                                    <li>
-                                        <!-- inner menu: contains the actual data -->
-                                        <ul class="menu">
-                                            <li><!-- start message -->
-                                                <a href="#">
-                                                    <div class="pull-left">
-                                                        <img src="{{asset('dist/img/user2-160x160.jpg')}}" class="img-circle" alt="User Image">
-                                                    </div>
-                                                    <h4>
-                                                        Support Team
-                                                        <small><i class="fa fa-clock-o"></i> 5 mins</small>
-                                                    </h4>
-                                                    <p>Why not buy a new awesome theme?</p>
-                                                </a>
-                                            </li>
-                                            <!-- end message -->
-                                        </ul>
-                                    </li>
-                                    <li class="footer"><a href="#">See All Messages</a></li>
-                                </ul>
-                            </li>
-                            <!-- Notifications: style can be found in dropdown.less -->
-                            <li class="dropdown notifications-menu">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                    <i class="fa fa-bell-o"></i>
-                                    <span class="label label-warning">10</span>
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li class="header">You have 10 notifications</li>
-                                    <li>
-                                        <!-- inner menu: contains the actual data -->
-                                        <ul class="menu">
-                                            <li>
-                                                <a href="#">
-                                                    <i class="fa fa-users text-aqua"></i> 5 new members joined today
-                                                </a>
-                                            </li>
-                                        </ul>
-                                    </li>
-                                    <li class="footer"><a href="#">View all</a></li>
-                                </ul>
-                            </li>
-                            <!-- Tasks: style can be found in dropdown.less -->
-                            <li class="dropdown tasks-menu">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                    <i class="fa fa-flag-o"></i>
-                                    <span class="label label-danger">9</span>
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li class="header">You have 9 tasks</li>
-                                    <li>
-                                        <!-- inner menu: contains the actual data -->
-                                        <ul class="menu">
-                                            <li><!-- Task item -->
-                                                <a href="#">
-                                                    <h3>
-                                                        Design some buttons
-                                                        <small class="pull-right">20%</small>
-                                                    </h3>
-                                                    <div class="progress xs">
-                                                        <div class="progress-bar progress-bar-aqua" style="width: 20%" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
-                                                            <span class="sr-only">20% Complete</span>
-                                                        </div>
-                                                    </div>
-                                                </a>
-                                            </li>
-                                            <!-- end task item -->
-                                        </ul>
-                                    </li>
-                                    <li class="footer">
-                                        <a href="#">View all tasks</a>
-                                    </li>
-                                </ul>
-                            </li>
+                            
                             <!-- User Account: style can be found in dropdown.less -->
                             <li class="dropdown user user-menu">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                    <img src="dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
-                                    <span class="hidden-xs">{{auth()->user()->name}}</span>
+                                    <span class="hidden-xs">Signed in as: {{auth()->user()->name}}</span>
                                 </a>
                                 <ul class="dropdown-menu">
                                     <!-- User image -->
                                     <li class="user-header">
-                                        <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-
                                         <p>
-                                            {{auth()->user()->email}}
-                                            <small>Member since Nov. 2012</small>
+                                            Email: {{auth()->user()->email}}
                                         </p>
                                     </li>
                                     <!-- Menu Body -->
                                     <li class="user-body">
                                         <div class="row">
-                                            <div class="col-xs-4 text-center">
+                                          {{--   <div class="col-xs-4 text-center">
                                                 <a href="#">Followers</a>
                                             </div>
                                             <div class="col-xs-4 text-center">
@@ -162,25 +77,19 @@
                                             </div>
                                             <div class="col-xs-4 text-center">
                                                 <a href="#">Friends</a>
-                                            </div>
+                                            </div> --}}
                                         </div>
                                         <!-- /.row -->
                                     </li>
                                     <!-- Menu Footer-->
                                     <li class="user-footer">
-                                        <div class="pull-left">
-                                            <a href="#" class="btn btn-default btn-flat">Profile</a>
-                                        </div>
                                         <div class="pull-right">
                                             <a href="{{ url('logout') }}" class="btn btn-default btn-flat">Sign out</a>
                                         </div>
                                     </li>
                                 </ul>
                             </li>
-                            <!-- Control Sidebar Toggle Button -->
-                            <li>
-                                <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
-                            </li>
+                          
                         </ul>
                     </div>
                 </nav>
@@ -192,18 +101,6 @@
             <aside class="main-sidebar">
                 <!-- sidebar: style can be found in sidebar.less -->
                 <section class="sidebar">                    
-                    <!-- search form -->
-                    <form action="#" method="get" class="sidebar-form">
-                        <div class="input-group">
-                            <input type="text" name="q" class="form-control" placeholder="Search...">
-                            <span class="input-group-btn">
-                                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
-                                </button>
-                            </span>
-                        </div>
-                    </form>
-                    <!-- /.search form -->
-                    <!-- sidebar menu: : style can be found in sidebar.less -->
                     @include('sidemenu')
                 </section>
                 <!-- /.sidebar -->
@@ -219,14 +116,14 @@
 
             <footer class="main-footer">
                 <div class="pull-right hidden-xs">
-                    <b>Version</b> 2.3.6
+                    <b>Version</b> 1.0
                 </div>
-                <strong>Copyright &copy; 2016 <a href="#">iERP System</a>.</strong> All rights
+                <strong>Copyright &copy; 2017 <a href="http://psionicinteractive.com" target="_blank"><img src="{{asset('dist/img/psionic_interactive_logo_2.jpg')}}" width="204" height="26"></a></strong> All rights
                 reserved.
             </footer>
 
             <!-- Control Sidebar -->
-            <aside class="control-sidebar control-sidebar-dark">
+            {{-- <aside class="control-sidebar control-sidebar-dark">
                 <!-- Create the tabs -->
                 <ul class="nav nav-tabs nav-justified control-sidebar-tabs">
                     <li><a href="#control-sidebar-home-tab" data-toggle="tab"><i class="fa fa-home"></i></a></li>
@@ -414,7 +311,7 @@
                     </div>
                     <!-- /.tab-pane -->
                 </div>
-            </aside>
+            </aside> --}}
             <!-- /.control-sidebar -->
             <!-- Add the sidebar's background. This div must be placed
             immediately after the control sidebar -->
--- resources/views/sidemenu.blade.php
+++ resources/views/sidemenu.blade.php
@@ -39,47 +39,6 @@
                     <li><a href="{{url('permissions')}}"><i class="fa fa-circle-o"></i> Permission</a></li>                    
                 </ul>
             </li>
-
-            <li>
-                <a href="#"><i class="fa fa-circle-o"></i> Companies
-                    <span class="pull-right-container">
-                        <i class="fa fa-angle-left pull-right"></i>
-                    </span>
-                </a>
-                <ul class="treeview-menu">
-                    <li><a href="{{url('companyinfo')}}"><i class="fa fa-circle-o"></i> Company Info</a></li>
-                    <li><a href="{{url('branches')}}"><i class="fa fa-circle-o"></i> Branches</a></li>                    
-                </ul>
-            </li>
         </ul>
     </li>
-    <li class="treeview">
-        <a href="#">
-            <i class="fa fa-laptop"></i>
-            <span>Products</span>
-            <span class="pull-right-container">
-                <i class="fa fa-angle-left pull-right"></i>
-            </span>
-        </a>
-        <ul class="treeview-menu">
-            <li><a href=""><i class="fa fa-circle-o"></i> Product List</a></li>
-            <li><a href=""><i class="fa fa-circle-o"></i> New Product</a></li>
-            <li><a href=""><i class="fa fa-circle-o"></i> Product Group</a></li>
-            <li><a href=""><i class="fa fa-circle-o"></i> Product Sub-Group</a></li>            
-        </ul>
-    </li>
-    <li class="treeview">
-        <a href="#">
-            <i class="fa fa-edit"></i> <span>Accounts</span>
-            <span class="pull-right-container">
-                <i class="fa fa-angle-left pull-right"></i>
-            </span>
-        </a>
-        <ul class="treeview-menu">
-            <li><a href="{{url('chart_of_ac')}}"><i class="fa fa-circle-o"></i> Chart Of Accounts</a></li>
-            <li><a href=""><i class="fa fa-circle-o"></i> Transactions</a></li>
-            <li><a href=""><i class="fa fa-circle-o"></i> Editors</a></li>
-        </ul>
-    </li>
-
 </ul>
